a8adb8941135667cfab339e64cc031d797bd91e7
velor2012 <38395332+velor2012@users.noreply.github.com>
2025-04-15 01:26:49 +0800

    init

---
+++ plugins.v2/downloadertagadder/__init__.py
@@ -0,0 +1,247 @@
+import datetime
+import threading
+from typing import List, Tuple, Dict, Any, Optional
+
+import pytz
+from app.helper.sites import SitesHelper
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.cron import CronTrigger
+from app.core.config import settings
+from app.helper.downloader import DownloaderHelper
+from app.log import logger
+from app.plugins import _PluginBase
+from app.schemas import ServiceInfo
+from app.utils.string import StringUtils
+from app.plugins.downloaderhelper.module import TaskContext
+from app.schemas.types import EventType, ChainEventType
+from app.core.event import eventmanager, Event
+
+class DownloaderTagAdder(_PluginBase):
+    # 插件名称
+    plugin_name = "下载器添加标签"
+    # 插件描述
+    plugin_desc = "给qb、tr的下载器贴标签（下载时触发）"
+    # 插件图标
+    plugin_icon = "https://raw.githubusercontent.com/hotlcc/MoviePilot-Plugins-Third/main/icons/DownloaderHelper.png"
+    # 插件版本
+    plugin_version = "0.0.1"
+    # 插件作者
+    plugin_author = "velor2012"
+    # 作者主页
+    author_url = "https://github.com/velor2012"
+    # 插件配置项ID前缀
+    plugin_config_prefix = "Downloader_Tag_Adder"
+    # 加载顺序
+    plugin_order = 21
+    # 可使用的用户级别
+    auth_level = 2
+    # 日志前缀
+    LOG_TAG = "[DownloaderTagAdder]"
+
+    # 退出事件
+    _event = threading.Event()
+    # 私有属性
+    downloader_helper = None
+    _scheduler = None
+    _enabled = False
+    _downloaders = None
+    _downloader_configs = {}  # 下载器独立配置，存储自定义标签
+
+    def init_plugin(self, config: dict = None):
+        self.downloader_helper = DownloaderHelper()
+        # 读取配置
+        if config:
+            self._enabled = config.get("enabled")
+            self._downloaders = config.get("downloaders")
+            # 读取下载器自定义标签配置
+            self._downloader_configs = {}
+            for dl in self.downloader_helper.get_configs().values():
+                if f"{dl.name}_custom_tags" in config:
+                    self._downloader_configs[dl.name] = {
+                        "custom_tags": config.get(f"{dl.name}_custom_tags", "").split("\n")
+                    }
+    @eventmanager.register(EventType.DownloadAdded)
+    @eventmanager.register(ChainEventType.ResourceDownload)
+    def listen_download_added_event(self, event: Event = None):
+        """
+        监听下载添加事件
+        """
+        logger.info('监听到下载添加事件')
+        if not self.get_state() :
+            logger.warn('插件状态无效或未开启监听，忽略事件')
+            return
+        if not event or not event.event_data:
+            logger.warn('事件信息无效，忽略事件')
+            return
+        # 执行
+        logger.info('下载添加事件监听任务执行开始...')
+        # enable_seeding=True是针对辅种添加种子并跳过校验的场景
+        context = event.event_data.get("context")
+        _hash = event.event_data.get('hash')
+        downloader = event.event_data.get("downloader")
+        service = self.downloader_helper.get_service(downloader)
+        downloader_obj = service.instance
+        if not service or not downloader_obj:
+            logger.error(f"未获取到下载器：{downloader}")
+            return
+        if downloader_obj.is_inactive():
+            logger.error(f"下载器 {downloader} 不在线")
+            return
+        
+        # 获取下载器配置中的自定义标签
+        tag = []
+        logger.info(f"获取下载器 {downloader} 自定义标签")
+        logger.info(f'配置为{self._downloader_configs}')
+        if downloader in self._downloader_configs:
+            tag.extend(self._downloader_configs[downloader].get("custom_tags", []))
+        
+        if service.type == "qbittorrent":
+            downloader_obj.set_torrents_tag(ids=_hash, tags=tag)
+            if service.type == "transmission":
+                # 由于 tr 会覆盖原标签，此处设置追加
+                _tags = downloader_obj.get_torrent_tags(ids=_hash)
+                downloader_obj.set_torrent_tag(ids=_hash, tags=tag, org_tags=_tags)
+
+        logger.info('下载添加事件监听任务执行结束')
+
+
+    def get_form(self) -> Tuple[List[dict], Dict[str, Any]]:
+        # 获取所有下载器配置
+        downloader_configs = self.downloader_helper.get_configs()
+        downloader_options = [{"title": dl.name, "value": dl.name} for dl in downloader_configs.values()]
+        
+        # 构建下载器配置表单
+        downloader_forms = []
+        for dl in downloader_configs.values():
+            downloader_forms.append({
+                "component": "VRow",
+                "content": [
+                    {
+                        "component": "VCol",
+                        "props": {"cols": 12},
+                        "content": [
+                            {
+                                "component": "VTextarea",
+                                "props": {
+                                    "model": f"{dl.name}_custom_tags",
+                                    "label": f"{dl.name} - 自定义标签",
+                                    "rows": 3,
+                                    "placeholder": "每行一个标签\n例如：\n电影\n剧集\n动漫",
+                                },
+                            }
+                        ]
+                    }
+                ]
+            })
+
+        return [
+            {
+                'component': 'VForm',
+                'content': [
+                    {
+                        'component': 'VRow',
+                        'content': [
+                            {
+                                'component': 'VCol',
+                                'props': {
+                                    'cols': 12,
+                                    # 'md': 6
+                                },
+                                'content': [
+                                    {
+                                        'component': 'VSwitch',
+                                        'props': {
+                                            'model': 'enabled',
+                                            'label': '启用插件',
+                                        }
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        'component': 'VRow',
+                        'content': [
+                            {
+                                'component': 'VCol',
+                                'props': {'cols': 12},
+                                'content': [
+                                    {
+                                        'component': 'VSelect',
+                                        'props': {
+                                            'multiple': True,
+                                            'chips': True,
+                                            'clearable': True,
+                                            'model': 'downloaders',
+                                            'label': '下载器',
+                                            'items': downloader_options
+                                        }
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    # 添加下载器独立配置表单
+                    *downloader_forms,
+                    {
+                        'component': 'VRow',
+                        'content': [
+                            {
+                                'component': 'VCol',
+                                'props': {
+                                    'cols': 12
+                                },
+                                'content': [
+                                    {
+                                        'component': 'VAlert',
+                                        'props': {
+                                            'type': 'info',
+                                            'variant': 'tonal',
+                                            'text': '每行配置一个，只会匹配一个，行数越高优先级越高。注意！！需用英文的:。'
+                                        }
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ]
+            }
+        ], {
+            "enabled": False,
+            "onlyonce": False,
+            "cover": False,
+            "site_first": False,
+            # 初始化下载器自定义标签配置
+            **{f"{dl.name}_custom_tags": "" for dl in downloader_configs.values()}
+        }
+
+    def get_state(self) -> bool:
+        return self._enabled
+
+    @staticmethod
+    def get_command() -> List[Dict[str, Any]]:
+        """
+        定义远程控制命令
+        :return: 命令关键字、事件、描述、附带数据
+        """
+        pass
+
+    def get_api(self) -> List[Dict[str, Any]]:
+        """
+        获取插件API
+        """
+        pass
+
+
+    def get_page(self) -> List[dict]:
+        pass
+
+    def stop_service(self):
+        """
+        退出插件
+        """
+        try:
+            logger.info('尝试停止插件服务...')
+            logger.info('插件服务停止完成')
+        except Exception as e:
+            logger.error(f"插件服务停止异常: {str(e)}", exc_info=True)
---
+++ plugins.v2/downloadtorrentplus/__init__.py
@@ -0,0 +1,437 @@
+from typing import Any, List, Dict, Tuple, Optional
+
+from app.core.event import eventmanager, Event
+from app.db.site_oper import SiteOper
+from app.plugins import _PluginBase, tag
+from app.log import logger
+from app.schemas.types import EventType
+from app.utils.string import StringUtils
+from app.schemas import ServiceInfo
+from app.helper.downloader import DownloaderHelper
+from app.helper.directory import DirectoryHelper
+
+# fork from https://github.com/thsrite/MoviePilot-Plugins/tree/main/plugins.v2/downloadtorrent
+class DownloadTorrentPlus(_PluginBase):
+    # 插件名称
+    plugin_name = "添加种子下载"
+    # 插件描述
+    plugin_desc = "选择下载器，添加种子任务。"
+    # 插件图标
+    plugin_icon = "download.png"
+    # 插件版本
+    plugin_version = "0.1"
+    # 插件作者
+    plugin_author = "velor2012"
+    # 作者主页
+    author_url = "https://github.com/velor2012"
+    # 插件配置项ID前缀
+    plugin_config_prefix = "Download_Torrent_Plus"
+    # 加载顺序
+    plugin_order = 28
+    # 可使用的用户级别
+    auth_level = 1
+
+    # 私有属性
+    _is_paused = False
+    _enabled = False
+    _save_path = None
+    _mp_path = None
+    _downloader = None
+    site = None
+    torrent_helper = None
+    downloader_helper = None
+    directory_helper = None
+
+    def init_plugin(self, config: dict = None):
+        self.downloader_helper = DownloaderHelper()
+        self.directory_helper = DirectoryHelper()
+        self.site = SiteOper()
+
+        if config:
+            self._enabled = config.get("enabled")
+            self._is_paused = config.get("is_paused")
+            self._save_path = config.get("save_path")
+            self._mp_path = config.get("mp_path")
+            self._torrent_urls = config.get("torrent_urls")
+            self._custom_tags = config.get("custom_tags")
+            self._custom_category = config.get("custom_category")
+            self._downloader = config.get("downloader")
+
+        
+            # 下载种子
+            if self._torrent_urls:
+                for torrent_url in str(self._torrent_urls).split("\n"):
+                    self.__download_torrent(torrent_url)
+
+
+            self.update_config({
+                "downloader": self._downloader,
+                "save_path": self._save_path,
+                "enabled": self._enabled,
+                "mp_path": self._mp_path,
+                "is_paused": self._is_paused
+            })
+
+    def __download_torrent(self, torrent_url: str):
+        """
+        下载种子
+        """
+        # 获取种子对应站点cookie
+        domain = StringUtils.get_url_domain(torrent_url)
+        if not domain:
+            logger.error(f"种子 {torrent_url} 获取站点域名失败，跳过处理")
+            return None, None
+
+        site = None
+        try:
+            # 查询站点
+            site = self.site.get_by_domain(domain)
+            if not site or not site.cookie:
+                logger.warning(f"种子 {torrent_url} 获取站点cookie失败，跳过处理")
+                # return None, None
+        except Exception as e:
+            logger.error(f"种子 {torrent_url} 获取站点cookie失败，跳过处理 {e}")
+            return None, None
+            
+        tags =  str(self._custom_tags).split(",") if self._custom_tags else []
+        category = self._custom_category if self._custom_category else None
+        service = self.service_info(self._downloader)
+        download_id = self.__download(service=service,
+                                      content=torrent_url,
+                                      save_path=self._save_path or self._mp_path,
+                                      cookie=site.cookie if site else 'site.cookie',
+                                      tags=tags,
+                                      category=category
+                                      )
+
+        if download_id:
+            logger.info(f"种子添加下载成功 {torrent_url} 保存位置 {self._save_path or self._mp_path}")
+            return 'site.name', f"种子添加下载成功, 保存位置 {self._save_path or self._mp_path}"
+        else:
+            logger.error(f"种子添加下载失败 {torrent_url} 保存位置 {self._save_path or self._mp_path}")
+            return 'site.name', f"种子添加下载失败, 保存位置 {self._save_path or self._mp_path}"
+
+    @eventmanager.register(EventType.PluginAction)
+    def remote_sync_one(self, event: Event = None):
+        if event:
+            event_data = event.event_data
+            if not event_data or event_data.get("action") != "download_torrent":
+                return
+            args = event_data.get("arg_str")
+            if not args:
+                logger.error(f"缺少参数：{event_data}")
+                return
+
+            site_name, result = self.__download_torrent(args)
+            if not result:
+                self.post_message(channel=event.event_data.get("channel"),
+                                  title="添加种子下载失败",
+                                  userid=event.event_data.get("user"))
+            else:
+                self.post_message(channel=event.event_data.get("channel"),
+                                  title=f"{site_name} {result}",
+                                  userid=event.event_data.get("user"))
+
+    def service_info(self, name: str) -> Optional[ServiceInfo]:
+        """
+        服务信息
+        """
+        if not name:
+            logger.warning("尚未配置下载器，请检查配置")
+            return None
+
+        service = self.downloader_helper.get_service(name)
+        if not service or not service.instance:
+            logger.warning(f"获取下载器 {name} 实例失败，请检查配置")
+            return None
+
+        if service.instance.is_inactive():
+            logger.warning(f"下载器 {name} 未连接，请检查配置")
+            return None
+        return service
+
+    def __download(self, service: ServiceInfo, content: str,
+                   save_path: str, cookie: str, category: str, tags: List[str]) -> Optional[str]:
+        """
+        添加下载任务
+        """
+        if not service or not service.instance:
+            return
+        downloader = service.instance
+        if self.downloader_helper.is_downloader("qbittorrent", service=service):
+            # 判断是否已存在
+            torrent = downloader.add_torrent(content=content,
+                                             download_dir=save_path,
+                                             is_paused=self._is_paused,
+                                             tag=tags,
+                                             category=category,
+                                             cookie=cookie)
+            if not torrent:
+                return None
+            else:
+                return torrent
+        elif self.downloader_helper.is_downloader("transmission", service=service):
+            # 添加任务
+            torrent = downloader.add_torrent(content=content,
+                                             download_dir=save_path,
+                                             is_paused=self._is_paused,
+                                             tag=tags,
+                                             category=category,
+                                             cookie=cookie)
+            if not torrent:
+                return None
+            else:
+                return torrent.hashString
+
+        logger.error(f"不支持的下载器类型")
+        return None
+
+    def get_state(self) -> bool:
+        return self._enabled
+
+    @staticmethod
+    def get_command() -> List[Dict[str, Any]]:
+        return [
+            {
+                "cmd": "/dt",
+                "event": EventType.PluginAction,
+                "desc": "种子下载",
+                "category": "",
+                "data": {
+                    "action": "download_torrent"
+                }
+            }
+        ]
+
+    def get_api(self) -> List[Dict[str, Any]]:
+        pass
+
+    def get_form(self) -> Tuple[List[dict], Dict[str, Any]]:
+        """
+        拼装插件配置页面，需要返回两块数据：1、页面配置；2、数据结构
+        """
+        dir_conf = [{'title': d.name, 'value': d.download_path} for d in
+                    self.directory_helper.get_local_download_dirs()]
+        downloader_options = [{"title": config.name, "value": config.name} for config in
+                              self.downloader_helper.get_configs().values()]
+        return [
+            {
+                'component': 'VForm',
+                'content': [
+                    {
+                        'component': 'VRow',
+                        'content': [
+                            {
+                                'component': 'VCol',
+                                'props': {
+                                    'cols': 12,
+                                    'md': 4
+                                },
+                                'content': [
+                                    {
+                                        'component': 'VSwitch',
+                                        'props': {
+                                            'model': 'enabled',
+                                            'label': '启用插件',
+                                        }
+                                    }
+                                ]
+                            },
+                        ]
+                    },
+                    {
+                        'component': 'VRow',
+                        'content': [
+                            {
+                                'component': 'VCol',
+                                'props': {
+                                    'cols': 12,
+                                    'md': 3
+                                },
+                                'content': [
+                                    {
+                                        'component': 'VSelect',
+                                        'props': {
+                                            'model': 'downloader',
+                                            'label': '下载器',
+                                            'items': downloader_options
+                                        }
+                                    }
+                                ]
+                            },
+                            {
+                                'component': 'VCol',
+                                'props': {
+                                    'cols': 12,
+                                    'md': 3
+                                },
+                                'content': [
+                                    {
+                                        'component': 'VSelect',
+                                        'props': {
+                                            'model': 'is_paused',
+                                            'label': '暂停种子',
+                                            'items': [
+                                                {'title': '开启', 'value': True},
+                                                {'title': '不开启', 'value': False}
+                                            ]
+                                        }
+                                    }
+                                ]
+                            },
+                            {
+                                'component': 'VCol',
+                                'props': {
+                                    'cols': 12,
+                                    'md': 3
+                                },
+                                'content': [
+                                    {
+                                        'component': 'VSelect',
+                                        'props': {
+                                            'model': 'mp_path',
+                                            'label': 'MoviePilot保存路径',
+                                            'items': dir_conf
+                                        }
+                                    }
+                                ]
+                            },
+                            {
+                                'component': 'VCol',
+                                'props': {
+                                    'cols': 12,
+                                    'md': 3
+                                },
+                                'content': [
+                                    {
+                                        'component': 'VTextField',
+                                        'props': {
+                                            'model': 'save_path',
+                                            'label': '自定义保存路径'
+                                        }
+                                    }
+                                ]
+                            },
+                        ]
+                    },
+                    {
+                        'component': 'VRow',
+                        'content': [
+                            {
+                                'component': 'VCol',
+                                'props': {
+                                    'cols': 12,
+                                },
+                                'content': [
+                                    {
+                                        'component': 'VTextarea',
+                                        'props': {
+                                            'model': 'torrent_urls',
+                                            'rows': '3',
+                                            'label': '种子链接',
+                                            'placeholder': '种子链接，一行一个'
+                                        }
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        "component": "VRow",
+                        "content": [
+                            {
+                                "component": "VCol",
+                                "props": {"cols": 12},
+                                "content": [
+                                    {
+                                        "component": "VTextField",
+                                        "props": {
+                                            "model": f"custom_tags",
+                                            "label": f"自定义标签",
+                                            "placeholder": "种子链接，逗号(,)分隔",
+                                        },
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        "component": "VRow",
+                        "content": [
+                            {
+                                "component": "VCol",
+                                "props": {"cols": 12},
+                                "content": [
+                                    {
+                                        "component": "VTextField",
+                                        "props": {
+                                            "model": f"custom_category",
+                                            "label": f"自定义分类"
+                                        },
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        'component': 'VRow',
+                        'content': [
+                            {
+                                'component': 'VCol',
+                                'props': {
+                                    'cols': 12,
+                                },
+                                'content': [
+                                    {
+                                        'component': 'VAlert',
+                                        'props': {
+                                            'type': 'info',
+                                            'variant': 'tonal',
+                                            'text': '自定义保存路径优先级高于MoviePilot保存路径。'
+                                        }
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        'component': 'VRow',
+                        'content': [
+                            {
+                                'component': 'VCol',
+                                'props': {
+                                    'cols': 12,
+                                },
+                                'content': [
+                                    {
+                                        'component': 'VAlert',
+                                        'props': {
+                                            'type': 'info',
+                                            'variant': 'tonal',
+                                            'text': '保存路径为下载器保存路径，种子链接一行一个。'
+                                                    '添加的种子链接需站点已在站点管理维护或公共站点。'
+                                        }
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ]
+            }
+        ], {
+            "downloader": "qb",
+            "is_paused": False,
+            "enabled": False,
+            "save_path": "",
+            "mp_path": "",
+            "torrent_urls": ""
+        }
+
+    def get_page(self) -> List[dict]:
+        pass
+
+    def stop_service(self):
+        """
+        退出插件
+        """
+        pass
