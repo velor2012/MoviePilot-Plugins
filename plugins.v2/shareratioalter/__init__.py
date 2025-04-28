b72950575490ec3b5347d2bdabbc0e287d20c22a
velor2012 <38395332+velor2012@users.noreply.github.com>
2025-04-28 18:21:29 +0800

    更改版本号

--- plugins.v2/shareratioalter/__init__.py
+++ plugins.v2/shareratioalter/__init__.py
@@ -29,7 +29,7 @@ class ShareRatioAlter(_PluginBase):
     # 插件图标
     plugin_icon = "world.png"
     # 插件版本
-    plugin_version = "1.0.1"
+    plugin_version = "1.0.2"
     # 插件作者
     plugin_author = "velor2012"
     # 作者主页
